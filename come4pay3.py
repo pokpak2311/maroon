58715a23179f0d83a9193170d2ab30460188a313
prasertcbs <bluecotton.k@gmail.com>
2018-07-21 11:17:45 +0700

    initial commit

---
+++ come4pay3.py
@@ -0,0 +1,14 @@
+def comeXpayY(come_x, pay_y, per_head, pax):
+    """
+    มา m จ่าย n เช่น มา 4 จ่าย 3
+    :param come_x: มา
+    :param pay_y: จ่าย
+    :param per_head: ค่าอาหารต่อหัว
+    :param pax: จำนวนลูกค้าในโต๊ะ
+    :return: ค่าอาหาร
+    """
+    return (pax // come_x) * (pay_y * per_head) + (pax % come_x) * per_head
+
+if __name__ == '__main__':
+    print(comeXpayY(4, 3, 100, 5))
+
